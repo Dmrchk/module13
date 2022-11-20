c83bc82e1b77baa6bb8f917c0d25ba09f5f8b28a
Dmrchk <105112928+Dmrchk@users.noreply.github.com>
2022-11-20 12:05:28 +0100

    Delete module13/src directory

--- module13/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Tasks.firstTask();
-        Tasks.secondTask();
-        Tasks.thirdTask();
-
-    }
-}
--- module13/src/UserCreator.java
+++
@@ -1,9 +0,0 @@
-public class UserCreator {
-    public static User createUser() {
-        Address.Geo geo = new Address.Geo("123456", "123456");
-        Address address = new Address("Antonova", "2", "Kyiv", "03186", geo);
-        Company company = new Company("Sony pictures", "", "SonyP");
-
-        return new User(11, "Oleksandr", "Dymarchuk", "email@email.com", address, "123-456-789", "blabla.com", company);
-    }
-}
